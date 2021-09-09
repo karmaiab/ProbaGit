9a0aaabbb1d2b2d5f0689345ab677b750eaf01b6
Ekaterina Lyamina <svetlanka65@inbox.ru>
2021-09-09 13:18:57 +0300

    second commit

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -16,6 +16,7 @@ public class ProbaGit {
      */
     public static void main(String[] args) {
         System.out.println("First version of the code");
+        System.out.println("Second version of the code");
     }
     
 }
